e95f7a8f4ec3b1a5580f8f44ba55ccd856b27e22
Luscv <lucbarrospc@gmail.com>
2024-09-30 19:01:09 -0300

    fix locale lang

--- src/services/get-carrer.service.ts
+++ src/services/get-carrer.service.ts
@@ -3,6 +3,8 @@ import { db } from "../db";
 import { carrer } from "../db/schema";
 import { langRequest } from "../models/lang.interface";
 import dayjs from "dayjs";
+import 'dayjs/locale/pt-br'; // Importando o local pt-br
+import 'dayjs/locale/en'; // Garantindo que o inglês está disponível
 
 interface CarrerResponse {
     carrer: {
@@ -20,6 +22,8 @@ export async function getCarrer({lang}: langRequest): Promise<CarrerResponse>{
     const descriptionCol = lang === "en-US" ? carrer.descriptionEn : carrer.description
     const endIncompleteMsg = lang === "en-US" ? 'Present' : 'O momento'
 
+    const locale = lang === "en-US" ? 'en' : 'pt-br';
+
     const result = await db
         .select({
             title: titleCol,
@@ -34,8 +38,8 @@ export async function getCarrer({lang}: langRequest): Promise<CarrerResponse>{
         .orderBy(desc(carrer.startDate))
 
         const formattedResult = result.map(item => {
-            const startDateFormatted = dayjs(item.startDate).format("MMMM/YYYY"); 
-            const endDateFormatted = item.endDate ? dayjs(item.endDate).format("MMMM/YYYY") : endIncompleteMsg;
+            const startDateFormatted = dayjs(item.startDate).locale(locale).format("MMM/YYYY"); 
+            const endDateFormatted = item.endDate ? dayjs(item.endDate).locale(locale).format("MMM/YYYY") : endIncompleteMsg;
     
             return {
                 title: item.title,
